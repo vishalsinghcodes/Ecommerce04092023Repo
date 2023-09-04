cbfb377ac4b98e60addfd2fc50e55e83a6ac16ea
Vishal <vishalsingh164@gmail.com>
2023-09-04 11:32:39 +0530

    2nd Commit

--- src/main/java/prac03092023/PageObjects/OrderHistoryPage.java
+++ src/main/java/prac03092023/PageObjects/OrderHistoryPage.java
@@ -1,13 +1,15 @@
 package prac03092023.PageObjects;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import org.testng.asserts.Assertion;
 import org.testng.asserts.SoftAssert;
 
 import prac03092023.Abstract.AbstractClass;
